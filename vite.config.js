24443a4437bebd4b355124831a2afac8cc3fff48
al387367 <al387367@uji.es>
2023-02-12 13:12:51 +0100

    Test 2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/* https://vitejs.dev/config/
+/* https://vitejs.dev/config/ */
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  "base": "/ninart-portfolio/"
 })
-*/
+
+/*
 export default {
 
   "base":"/ninart-portfolio/"
   
-  }
+  }*/
